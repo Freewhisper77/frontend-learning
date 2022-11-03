71bcdd8cda5448db1b1f7e32ada6c8b3a51569d5
Freewhisper77 <emmasddx@foxmail.com>
2022-11-03 17:05:55 +0800

    refactor:last exercise

--- js/Array Practice/abbreviate.js
+++ js/Array Practice/abbreviate.js
@@ -9,20 +9,19 @@ const input = 'Every developer likes to mix kubernetes and javascript';
 
 // 1. string.split(' '):string->array
 // 2. array.filter length>=4
-const x = input.split(' ');
-console.log(x);
-console.log(x[0].length); // 5
-const y = x.filter((v) => v.length >= 4);
-console.log(y); //[ 'Every', 'developer', 'likes', 'kubernetes', 'javascript' ]
-
 // 3.array.map()遍历 用getAbbreviate函数使之->每个单词的对应的缩写的array
 // 4.array.join() -> string
-const z = y.map(getAbbreviate).join(' ');
-console.log(z);
 
-function getAbbreviate(string) {
-  const length = string.length;
-  const firstLetter = string[0];
-  const lastLetter = string[length - 1];
-  return firstLetter + (length-2) + lastLetter;
-}
+const result = input.split(' ')
+  .filter((v) => v.length >= 4)
+  .map((str) => `${str[0]}${str.length -2}${str[str.length - 1]}`)
+  .join(' ');
+
+// function getAbbreviate(string) {
+//   const length = string.length;
+//   const firstLetter = string[0];
+//   const lastLetter = string[length - 1];
+//   return firstLetter + (length-2) + lastLetter;
+// }
+
+console.log(result); // E3y d7r l3s k8s j8t
